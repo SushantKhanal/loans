ada9b73a669cec71dced0204f7a2e2a432d2235f
SushantKhanal <dreamerking114@gmail.com>
2023-09-13 11:30:33 -0500

    @RequestHeader(eazybank-correlation-id) String correlationid added for tracing with gateway

--- src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -6,10 +6,7 @@ package com.eazybytes.loans.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.eazybytes.loans.config.LoansServiceConfig;
 import com.eazybytes.loans.model.Customer;
@@ -35,7 +32,7 @@ public class LoansController {
 	LoansServiceConfig loansConfig;
 
 	@PostMapping("/myLoans")
-	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+	public List<Loans> getLoansDetails(@RequestHeader("eazybank-correlation-id") String correlationid, @RequestBody Customer customer) {
 		System.out.println("Invoking Loans Microservice");
 		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
 		if (loans != null) {
